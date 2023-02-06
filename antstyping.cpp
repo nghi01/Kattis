0e995683a1de8e9e65dd2ccd51c4ad5f162664a4
nghi01 <nnguyen@oberlin.edu>
2023-02-06 16:17:11 -0500

    Create antstyping.cpp

---
+++ antstyping.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "stdlib.h"
+#include "stdio.h"
+#include <iostream>
+#include <vector>
+#include <utility>
+
+
+using namespace std;
+int main()
+{
+    // initializing the elements
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> arrit = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> placeholder{};
+    //All possible combinations of pairs of 2 numbers from above:
+    vector<pair<int,int>> couples{};
+    for (int i = 9; i > 0; i--){
+        for (int j = i - 1; j > 0; j--){
+            pair <int, int> p;
+            p.first = i;
+            p.second = j;
+            couples.push_back(p);
+        }
+    }
+    // Print to check
+    for (pair<int,int>& i : couples){
+        cout << i.first << " " << i.second << "\n";
+    }
+}
