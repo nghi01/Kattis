8ec91491b441bf2bc505b205b3a423f99f76a8fe
nghi01 <nnguyen@oberlin.edu>
2023-02-07 14:42:10 -0500

    Update antstyping.cpp

--- antstyping.cpp
+++ antstyping.cpp
@@ -1,31 +1,39 @@
 #include <iostream>
 #include "stdlib.h"
 #include "stdio.h"
-#include <iostream>
 #include <vector>
-#include <utility>
-
+#include <bits/stdc++.h>
 
 using namespace std;
+//Display elements of the array
+void display(vector<int> a, int n){
+    for(int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
     // initializing the elements
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> placeholder{};
     //All possible combinations of pairs of 2 numbers from above:
-    vector<pair<int,int>> couples{};
-    for (int i = 9; i > 0; i--){
-        for (int j = i - 1; j > 0; j--){
-            pair <int, int> p;
-            p.first = i;
-            p.second = j;
-            couples.push_back(p);
-        }
-    }
-    // Print to check
-    for (pair<int,int>& i : couples){
-        cout << i.first << " " << i.second << "\n";
-    }
+    // vector<pair<int,int>> couples{};
+    // for (int i = 9; i > 0; i--){
+    //     for (int j = i - 1; j > 0; j--){
+    //         pair <int, int> p;
+    //         p.first = i;
+    //         p.second = j;
+    //         couples.push_back(p);
+    //     }
+    // }
+    // // Print to check
+    // for (pair<int,int>& i : couples){
+    //     cout << i.first << " " << i.second << "\n";
+    // }
     //Bruteforcing
-    for (int i = 9; i )
+    do {
+        display(arr, 8);
+    } while (next_permutation(arr.begin(), arr.end()));
+    return 0;
 }
