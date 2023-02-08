60a1b3943130485abca61d0f72e381c1b7ddc256
nghi01 <nnguyen@oberlin.edu>
2023-02-07 22:48:43 -0500

    Update antstyping.cpp

--- antstyping.cpp
+++ antstyping.cpp
@@ -3,6 +3,7 @@
 #include "stdio.h"
 #include <vector>
 #include <bits/stdc++.h>
+#include <string>
 
 using namespace std;
 //Display elements of the array
@@ -13,7 +14,7 @@ void display(vector<int> a, int n){
     cout << endl;
 }
 
-void getIndex(vector<int> v, int K)
+int getIndex(vector<int> v, int K)
 {
     vector<int>::iterator it;
     it = find(v.begin(), v.end(), K);
@@ -25,12 +26,31 @@ void getIndex(vector<int> v, int K)
         return index;
     }
 }
+
+int StringToInt(char c){
+    int i = int(c) - 48;
+    return i;
+}
+
 int main()
 {
     // initializing the elements
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    string sequence;
+    int minSteps, transit, transits, count;
+    cin >> sequence;
+    minSteps = 100;
     do {
-        display(arr, 9);
+        transits = 0;
+        for (int i = 0; i < size(sequence) - 1; i++){
+            transit = abs(getIndex(arr, StringToInt(sequence[i])) - getIndex(arr, StringToInt(sequence[i+1])));
+            transits += transit;
+        }
+        count = size(sequence) + 2 * transits;
+        if (count < minSteps){
+            minSteps = count;
+        }
     } while (next_permutation(arr.begin(), arr.end()));
+    cout << minSteps;
     return 0;
 }
